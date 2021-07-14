c659b385a7d869a2709b29c3130559f06a8986bf
BratiPradhan <pradhanbrati@gmail.com>
2021-07-15 00:50:38 +0530

    feature: Rate the course

--- src/components/routes/Announcements.js
+++ src/components/routes/Announcements.js
@@ -3,7 +3,9 @@ import React from 'react'
 export default function Announcements() {
     return (
         <div>
-            announcements
+            <h1>No Announcements posted yet</h1>
+            <p>The instructor hasn't added any announcements to this video yet.</p>
+            <p>Announcements are used to inform you of updates or additions to the course. </p>
         </div>
     )
 }
--- src/components/routes/RateThisCourse.js
+++ src/components/routes/RateThisCourse.js
@@ -1,9 +1,41 @@
-import React from 'react'
+import React, { useState } from "react";
+import { FaStar } from "react-icons/fa";
+import { Container, Radio, Rating } from "../../StyledComponents/routes/RateThisCourse.style";
+const RateThisCourse = () => {
+const [rate, setRate] = useState(0);
+return (
+    <div>
+        <h1>How would you rate this course?</h1>
+        <p><strong>Select Rating</strong></p>
+        <Container>
+        {
+        [...Array(5)].map((item, index) => {
+            const givenRating = index + 1;
+            return (
+            <label>
+                <Radio
+                type="radio"
+                value={givenRating}
+                onClick={() => {
+                    setRate(givenRating);
+                }}
+                />
+                <Rating>
+                <FaStar
+                    color={
+                    givenRating < rate || givenRating === rate
+                        ? "000"
+                        : "rgb(192,192,192)"
+                    }
+                />
+                </Rating>
+            </label>
+            );
+        })
+        }
+        </Container>
+    </div>
+);
+};
 
-export default function RateThisCourse() {
-    return (
-        <div>
-            Rate this course
-        </div>
-    )
-}
+export default RateThisCourse;
