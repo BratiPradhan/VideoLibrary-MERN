cc8b1dd6e2fdf59702ce2d280d69862efd11ec05
BratiPradhan <pradhanbrati@gmail.com>
2021-07-15 18:33:41 +0530

    feat: Route:: Share This Course

---
+++ src/components/routes/Share.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import { useState, useEffect } from 'react'
+import {InputGroup, FormControl, Button} from 'react-bootstrap'
+
+export default function Share() {
+
+    const [url, setUrl] = useState('')
+
+    useEffect(() => {
+        setUrl('https://react-bootstrap.github.io/components/input-group/')
+    }, [url])
+
+    
+    return (
+        <div>
+            <h1>Share this course</h1>
+
+
+            <InputGroup className="mb-3">
+    <FormControl
+    value={url}
+    disabled
+      placeholder="Recipient's username"
+      aria-label="Recipient's username"
+      aria-describedby="basic-addon2"
+    />
+    <Button onClick={() =>  navigator.clipboard.writeText(url)} variant="outline-secondary" id="button-addon2">
+      Copy
+    </Button>
+  </InputGroup>
+        </div>
+    )
+}
--- src/components/routes/Video.js
+++ src/components/routes/Video.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Nav } from 'react-bootstrap'
+import { FaShare } from 'react-icons/fa'
 
 import VideoPlayer from '../layouts/VideoPlayer'
 
@@ -10,6 +11,7 @@ import Notes from './Notes'
 import Announcements from './Announcements'
 import YourProgress from './YourProgress'
 import RateThisCourse from './RateThisCourse'
+import Share from './Share'
 
 export default function Video() {
     return (
@@ -38,6 +40,10 @@ export default function Video() {
                     <Nav.Item>
                         <NavLink activeClassName="active" className="nav-link" to="/video/rate-this-course">Rate this course</NavLink>
                     </Nav.Item>
+
+                    <Nav.Item>
+                        <NavLink activeClassName="active" className="nav-link" to="/video/share">Share <FaShare/></NavLink>
+                    </Nav.Item>
                 </Nav>
 
                 <Routes>
@@ -46,6 +52,7 @@ export default function Video() {
                     <Route path="/announcements" element={<Announcements/>}></Route>
                     <Route path="/your-progress" element={<YourProgress/>}></Route>
                     <Route path="/rate-this-course" element={<RateThisCourse/>}></Route>
+                    <Route path="/share" element={<Share/>}></Route>
                 </Routes>
 
             
